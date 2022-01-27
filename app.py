39137756856f387f8dd0e776e697e4f3a8975f66
ivanwu0405 <83348186+ivanwu0405@users.noreply.github.com>
2022-01-27 21:57:44 +0800

    Finish website v1.0

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import render_template, redirect, request, url_for, flash, abort
-from flask_login import login_user, logout_user, login_required
+from flask_login import current_user, login_user, logout_user, login_required
+from sqlalchemy import null
 from content import db,app
 from content.models import User
 from content.forms import RegistrationForm,LoginForm
+from werkzeug.utils import secure_filename
+import os
 db.create_all()
 @app.route('/')
 def home():
@@ -12,12 +15,12 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user.check_password(form.password.data) and user is not None:
+        if user is not None and user.check_password(form.password.data):
             login_user(user)
             flash("您已經成功的登入系統")
             next = request.args.get('next')
             if next == None or not next[0]=='/':
-                next = url_for('welcome_user')
+                next = url_for('step1')
             return redirect(next)
         flash("使用者或密碼錯誤")    
     return render_template('login.html',form=form)
@@ -43,9 +46,73 @@ def register():
         flash("感謝註冊本系統成為會員")
         return redirect(url_for('login'))
     return render_template('register.html',form=form)
-@app.route('/welcome')
+@app.route('/step1',methods=['GET','POST'])
 @login_required
-def welcome_user():
-    return render_template('welcome_user.html')
+def step1():
+    if request.method == 'POST': 
+        #flash("upload Successfully")
+        user=User.query.filter_by(username=current_user.username).first()
+        f=request.files['file']
+        basepath=os.path.dirname(__file__)
+        upload_path=os.path.join(basepath,'content/static',secure_filename(f.filename))
+        if upload_path == 'C:\\Users\\ivanw\\OneDrive\\桌面\\flaskWebsite\\content/static\\':
+            return render_template('welcome_user.html',size=350,img=None,op=None)
+        f.save(upload_path)
+        user.input_images('../static/'+f.filename)
+        db.session.commit()
+        return render_template('welcome_user.html',size=350,img='../static/'+f.filename)
+    return render_template('welcome_user.html',size=350,img=None,op=None)
+@app.route('/step2',methods=['GET','POST'])
+@login_required
+def step2():
+    if request.method=='POST':
+        user=User.query.filter_by(username=current_user.username).first()
+        o=request.values['optradio']
+        d=request.form.getlist('a')
+        if o== null:
+            return render_template('welcome_user.html',size=350,img=None,op=None)
+        elif o=='1':
+            
+            if d==['delete']:
+                print('d1:',d)
+                user.delete_image(1)
+                db.session.commit()
+            user.showing_num='1'
+            return render_template('welcome_user.html',size=350,img=user.image_path_1,op=o)
+        elif o=='2':
+            
+            if d==['delete']:
+                print('d2:',d)
+                user.delete_image(2)
+                db.session.commit()
+            user.showing_num='2'
+            return render_template('welcome_user.html',size=350,img=user.image_path_2,op=o)
+        else:
+            
+            if d==['delete']:
+                print('d3:',d)
+                user.delete_image(3)
+                db.session.commit()
+            user.showing_num='3'
+            return render_template('welcome_user.html',size=350,img=user.image_path_3,op=o)
+    return render_template('welcome_user.html',size=350,img=None,op=None)
+@app.route('/step3',methods=['GET','POST'])
+@login_required
+def step3():
+    if request.method=='POST':
+        user=User.query.filter_by(username=current_user.username).first()
+        s=request.values['ad-size']
+        if user.showing_num==0:
+            return render_template('welcome_user.html',size=s,img=None,op=None)
+        elif user.showing_num=='1':
+            return render_template('welcome_user.html',size=s,img=user.image_path_1,op=None)
+        elif user.showing_num=='2':
+            return render_template('welcome_user.html',size=s,img=user.image_path_2,op=None)
+        else:
+            return render_template('welcome_user.html',size=s,img=user.image_path_3,op=None)
+    return render_template('welcome_user.html',size=350,img=None,op=None)
+@app.route('/tutorial')
+def tutorial():
+    return render_template('how_to_use.html')
 if __name__ == '__main__':
     app.run(debug=True)
--- content/models.py
+++ content/models.py
@@ -1,14 +1,20 @@
+from operator import index
 from numpy import False_
+from sqlalchemy import null
 from content import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import json
 #import cv2
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    image_path    = db.Column(db.String(64))
+    image_path_1    = db.Column(db.String(64))
+    image_path_2    = db.Column(db.String(64))
+    image_path_3    = db.Column(db.String(64))
     id       = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64),unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    showing_num='0'
     def __init__(self, username, password):
         """初始化"""
         self.username = username
@@ -17,8 +23,29 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         """檢查使用者密碼"""
         return check_password_hash(self.password_hash, password)
-    def input_image(self,image_path):
-         self.image_path=image_path
+    def input_images(self,image_path):
+        if self.image_path_1==None:
+            print('I save image_path_1')
+            self.image_path_1=image_path
+            return
+        elif self.image_path_2==None:
+            print("I asve Image_path_2")
+            self.image_path_2=image_path
+            return
+        else:
+            print("I save Image_path_3")
+            self.image_path_3=image_path
+            return
+    def delete_image(self,image_num):
+        if image_num ==1:
+            self.image_path_1=None
+            return
+        elif image_num==2:
+            self.image_path_2=None
+            return
+        else:
+            self.image_path_3=None
+            return
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
